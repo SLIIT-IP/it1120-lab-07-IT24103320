09543abd55d0aadc3f50cbda1eded90b49863b89
IT24103320 <it24103320@my.sliit.lk>
2024-09-20 11:28:46 +0530

    Add files via upload

---
+++ Q2/IT24103320Lab7Q2A.java
@@ -0,0 +1,21 @@
+public class IT24103320Lab7Q2A{
+	public static void main (String args[]) {
+
+		
+		int rows = 4;
+		int columns = 5;
+
+		for (int rowCount = 1; rowCount <= rows; rowCount++) {
+		
+		  for (int columnCount = 1; columnCount <= columns; columnCount++) {
+
+		  System.out.print("$ ");
+
+		  }
+		  
+		  System.out.println();
+		}
+
+	}
+}
+		
